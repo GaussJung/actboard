39867bfd8433f0c4495e04a8d3c5b165b377b206
kyh <haa3163314@naver.com>
2021-07-12 11:39:00 +0900

    test

--- ecosystem.config.js
+++
@@ -1,9 +0,0 @@
-﻿module.exports = {
-    apps: [{
-        name: 'actboard',
-        script: './server.js',
-        instances: 0,
-        exec_mode: 'cluster',
-        merge_logs: true
-    }]
-}
