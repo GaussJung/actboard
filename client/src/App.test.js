1cc7ec7d31f02c2c3f376db9015af30c27fc1064
yeonghoon123 <haa3163314@naver.com>
2021-07-12 15:42:32 +0900

    test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+require('actboard/server/npm start')
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
