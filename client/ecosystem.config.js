c827b09effd412b88fa677669e175c61b32235ce
yeonghoon123 <haa3163314@naver.com>
2021-07-13 17:02:01 +0900

    test

--- client/ecosystem.config.js
+++ client/ecosystem.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     "apps": [{
             name : "actboard_client",
-            script : "npm start",
+            script : "npm",
+            args : 'start',
             instances: 0,
             exec_mode: 'cluster'
         }
