6d17f6a82013e8d85d1ee2dd8a8a829ffa028803
yeonghoon123 <haa3163314@naver.com>
2021-07-13 16:43:38 +0900

    test

--- client/ecosystem.config.js
+++ client/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     "apps": [{
         
             name : "actboard_client",
-            script : "node_modules/.bin/react-scripts",
+            script : "node_modules/react-scripts/scripts/start.js",
             instances: 0,
             exec_mode: 'cluster'
         }
