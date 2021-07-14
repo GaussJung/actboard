6fee6fe10bff4e1428be55abf87136e4f39aa03a
yeonghoon123 <haa3163314@naver.com>
2021-07-14 12:08:47 +0900

    test

--- client/src/api.js
+++ client/src/api.js
@@ -1,18 +1,9 @@
 import axios from "axios";
 
-let apiServer = "";
-const currentUrl = document.location.href;
-
-if (currentUrl.indexOf("localhost") > -1) {
-  apiServer = "http://localhost:8000";
-} else {
-  apiServer = "http://utdev.soymlops.com:8000";
-}
-
 // 목록화면의 데이터 수집
 export const GetDbData = async () => {
   const dbList = await axios
-    .get(apiServer + "/api/exam/list")
+    .get("/api/exam/list")
     .then((res) => res.data);
 
   return dbList;
@@ -20,14 +11,14 @@ export const GetDbData = async () => {
 
 // 사용자 목록 데이터
 export const GetUserList = async () => {
-  const dbList = await axios.get(apiServer + "/api/user/userList").then((res) => res.data);
+  const dbList = await axios.get("/api/user/userList").then((res) => res.data);
 
   return dbList;
 };
 
 // 로그인상태일 경우 정보 저장
 export const CompleteLogin = async () => {
-  const dbList = await axios.get(apiServer + "/api/user/completelogin").then((res) => res.data);
+  const dbList = await axios.get("/api/user/completelogin").then((res) => res.data);
 
   return dbList;
 };
