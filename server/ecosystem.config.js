363d94fb9722a7cfae85d68517d0dc0b7ab19f08
yeonghoon123 <haa3163314@naver.com>
2021-07-14 10:11:07 +0900

    test

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,11 +1,10 @@
 module.exports = {
     "apps": [
         {
-            "name" : "actboard_server",
-            "script" : "./server.js",
-            "args" : "start",
-            "watch" : true,
-            "exec_mode": 'cluster'
+            name : "actboard_server",
+            script : "./server.js",
+            instances: 0,
+            exec_mode: 'cluster'
         }
     ]
 }
