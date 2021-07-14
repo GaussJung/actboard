99a02d54acd49bb4627fc18ddc5bf5aedd4539b0
yeonghoon123 <haa3163314@naver.com>
2021-07-14 09:55:12 +0900

    test

--- server/ssl_ecosystem.config.js
+++ server/ssl_ecosystem.config.js
@@ -1,11 +1,10 @@
 module.exports = {
     "apps": [
         {
-            "name" : "actboard_server",
-            "script" : "./sslserver.js",
-            "args" : "start",
-            "watch" : true,
-            "exec_mode": 'cluster'
+            name : "actboard_sslserver",
+            script: "./sslserver.js",
+            instances: 0,
+            exec_mode: 'cluster'
         }
     ]
 }
